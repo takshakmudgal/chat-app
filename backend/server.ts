190c57a96214f6f90523df2cfe31c17b80b0ff8d
takshakmudgal <takshakmudgal@gmail.com>
2025-07-09 21:37:52 +0530

    chore: socket server init

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,24 @@
 import express from "express";
+import { Server } from "socket.io";
 import type { Request, Response } from "express";
 
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
 const port = process.env.BACKEND_PORT ?? 3000;
+
+const app = express();
+const io = new Server(
+  app.listen(port, () => {
+    console.log("serverrrawww!!");
+  })
+);
+
+io.on("connection", (socket) => {
+  console.log("connected to websocket");
+  socket.emit("connected");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hiii_welcome_to_your_express_server");
-});
-
-
-
-app.listen(port, () => {
-  console.log(`Server_Up_http://localhost:${port}`);
 });
