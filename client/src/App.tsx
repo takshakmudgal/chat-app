7bd072219385ced840f00158c25b0be6be857735
takshakmudgal <takshakmudgal@gmail.com>
2025-07-11 04:25:33 +0530

    feat: emit socket message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,26 @@ import "./index.css";
 
 import logo from "../public/logo.svg";
 import reactLogo from "../public/react.svg";
+import { io } from "socket.io-client";
+import { useState } from "react";
+
+const socket = io("ws://localhost:8080");
 
 export function App() {
+  const [input, setInput] = useState("");
+  const sendInput = () => {
+    socket.emit("send_message", input);
+  };
+
   return (
     <div className="app">
       <div className="logo-container">
         <img src={logo} alt="Bun Logo" className="logo bun-logo" />
         <img src={reactLogo} alt="React Logo" className="logo react-logo" />
       </div>
-
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMRr
-      </p>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <br />
+      <button onClick={sendInput}>sned!</button>
     </div>
   );
 }
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Server } from "socket.io";
+import cors from "cors";
 import type { Request, Response } from "express";
 
 import dotenv from "dotenv";
@@ -8,17 +9,28 @@ dotenv.config();
 const port = process.env.BACKEND_PORT ?? 8080;
 
 const app = express();
-const io = new Server(
-  app.listen(port, () => {
-    console.log(`server_hosted_at_http://localhost:${port}`);
-  })
-);
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST"],
+};
+
+app.use(cors(corsOptions));
+
+const expressServer = app.listen(port, () => {
+  console.log(`server_hosted_at_http://localhost:${port}`);
+});
+
+const io = new Server(expressServer, {
+  cors: corsOptions,
+});
 
 io.on("connection", (socket) => {
-  console.log("connected to websocket");
-  socket.emit("connected");
+  console.log(`websocket_hosted_at_ws://localhost:${port}`);
+  socket.on("send_message", (msg) => {
+    console.log(msg);
+  });
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hiii_welcome_to_your_express_server");
+  res.send("hiii_welcome_to_yoursdfs_expresssdfsd _server");
 });
